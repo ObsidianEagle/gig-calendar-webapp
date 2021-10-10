11d61189a8989481949f3ea419990b760a3f54dc
Alex King <alex.king@and.digital>
2021-10-10 20:29:50 +0100

    Display gigs retrieved from API
    To do: improve styling, add error handling (api failure, heroku cold start)
    Future: extend for use by multiple people

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,25 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	import ListItem from './ListItem.svelte';
+
+	export let gigs = [];
+
+	onMount(() => {
+		fetch('https://gig-calendar-node-api.herokuapp.com/')
+			.then((res) => res.json())
+			.then((data) => {
+				gigs = data.values.slice(1);
+			});
+	})
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Upcoming Gigs</h1>
+	<hr />
+	{#each gigs as gig (gig)}
+		<ListItem artist={gig[0]} venue={gig[1]} date={gig[2]} supports={gig[4]} />
+		<hr />
+	{/each}
 </main>
 
 <style>
@@ -13,10 +28,11 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		color: #ddd;
+		background-color: #1A1A1A;
 	}
 
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
---
+++ src/ListItem.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let artist;
+  export let supports;
+  export let venue;
+  export let date;
+</script>
+
+<div class="list-item">
+  <div>
+    <h2>{artist}</h2>
+    <h6>Supports: {supports}</h6>
+  </div>
+  <div>
+    <h4>{venue}</h4>
+  </div>
+  <div>
+    <h4>{date}</h4>
+  </div>
+</div>
+
+<style>
+  .list-item {
+    display: flex;
+    width: 100%;
+    align-items: center;
+  }
+
+  .list-item > div {
+    flex: 1;
+  }
+
+  h2 {
+    margin-bottom: 0;
+  }
+
+  h6 {
+    margin-top: 1.5em;
+  }
+</style>
