fb755846cda4e8ddcea976e69cd208cdbc513c54
Alex King <alex.king@and.digital>
2021-10-24 15:33:52 +0100

    Add loading spinner and error handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,23 @@
 <script>
 	import { onMount } from 'svelte';
 	import ListItem from './ListItem.svelte';
+	import LoadingSpinner from './LoadingSpinner.svelte';
 
-	export let gigs = [];
+	let gigs = [];
+	let loading = true;
+	let errorOccured = false;
 
 	onMount(() => {
 		fetch('https://gig-calendar-node-api.herokuapp.com/')
 			.then((res) => res.json())
 			.then((data) => {
 				gigs = data.values.slice(1);
+				loading = false;
+			})
+			.catch((e) => {
+				loading = false;
+				errorOccured = true;
+				console.error(e);
 			});
 	})
 </script>
@@ -16,10 +25,16 @@
 <main>
 	<h1>Upcoming Gigs</h1>
 	<hr />
-	{#each gigs as gig (gig)}
-		<ListItem artist={gig[0]} venue={gig[1]} date={gig[2]} supports={gig[4]} />
-		<hr />
-	{/each}
+	{#if loading}
+		<LoadingSpinner />
+	{:else if errorOccured}
+		<p>An error occured while fetching data</p>
+	{:else}
+		{#each gigs as gig (gig)}
+			<ListItem artist={gig[0]} venue={gig[1]} date={gig[2]} supports={gig[4]} />
+			<hr />
+		{/each}
+	{/if}
 </main>
 
 <style>
